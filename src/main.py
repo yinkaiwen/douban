83539c783089a553139d34c31c3675cf56c3cd9a
Administrator <Administrator@PC201508281040>
2016-11-03 08:51:27 +0800

    hehe

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ Created on 2016年11月2日
 
 @author: yinkaiwen
 '''
-from src.http.DouBanHtml import DouBanHtml
+from src.http import DouBanHtml
 
 if __name__ == '__main__':
     douban = DouBanHtml('中国');
